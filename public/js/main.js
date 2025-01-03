4ea3a687221169844d560aeefaa3e35fac800de7
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2025-01-03 17:54:49 +0000

    Update portfolio section titles and enhance styles for better presentation

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,3 @@
-// let dob = new Date("1995-02-02");
-// let currentDate = new Date();
-// let age = currentDate.getFullYear() - dob.getFullYear();
-// document.getElementById("age").innerText = age;
 (function () {
   "use strict";
   const select = (el, all = false) => {
@@ -19,32 +15,44 @@
   const onscroll = (el, listener) => {
     el.addEventListener("scroll", listener);
   };
-  let navbarlinks = select("#navbar .scrollto", true);
+  let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
-    let position = window.scrollY + 200;
-    navbarlinks.forEach((navbarlink) => {
-      if (!navbarlink.hash) return;
-      let section = select(navbarlink.hash);
-      if (!section) return;
-      if (
-        position >= section.offsetTop &&
-        position <= section.offsetTop + section.offsetHeight
-      ) {
-        navbarlink.classList.add("active");
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
       } else {
-        navbarlink.classList.remove("active");
+        navbarlink.classList.remove('active')
       }
-    });
-  };
-  window.addEventListener("load", navbarlinksActive);
-  onscroll(document, navbarlinksActive);
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
   const scrollto = (el) => {
-    let elementPos = select(el).offsetTop;
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
     window.scrollTo({
-      top: elementPos,
-      behavior: "smooth",
-    });
-  };
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
   let backtotop = select(".back-to-top");
   if (backtotop) {
     const toggleBacktotop = () => {
